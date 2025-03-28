ef2194d58a3c1802399437d53fcb61aa13e7b143
crisheil26 <altheacrisheild@gmail.com>
2025-03-29 00:36:42 +0800

    Create arrayMethodsActivity.js

---
+++ arrayMethodsActivity.js
@@ -0,0 +1,7 @@
+// Activity 2: Array Methods Practice
+
+// 1. Use concat() to join two arrays
+let arr1 = ["Cecile", "Lone"];
+let arr2 = ["Emil", "Tobias", "Linus"];
+let combinedArr = arr1.concat(arr2);
+console.log("Concatenated Array:", combinedArr);
