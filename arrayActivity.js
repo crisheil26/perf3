1ddc73592ea331408398f86c560318aac734432f
crisheil26 <altheacrisheild@gmail.com>
2025-03-29 00:28:30 +0800

    Create arrayActivity.js

---
+++ arrayActivity.js
@@ -0,0 +1,24 @@
+// Activity 1: Working with Arrays
+
+// 1. Create an array called favSingers and store three favorite singers
+let favSingers = ["Taylor Swift", "Ed Sheeran", "Adele"];
+console.log(favSingers);
+
+//2.search the index of favSingers
+let pos = favSinger.indexof(favSinger);
+console.log("First favorite singer:", favSingers[0]); // "Taylor Swift"
+console.log("Second favorite singer:", favSingers[1]); // "Ed Sheeran"
+console.log("Third favorite singer:", favSingers[2]); // "Adele"
+
+// 3. Create an array called favNumbers and store four favorite numbers
+let favNumbers = [7, 21, 42, 99];
+console.log(favNumbers);
+
+// 4. Create an array named mixedArr with different data types
+let mixedArr = ["string", ["otherarray"], 123, true];
+
+// 5. Access and log each item in the mixedArr using array index notation
+console.log("First item in mixedArr (string):", mixedArr[0]); // "string"
+console.log("Second item in mixedArr (nested array):", mixedArr[1]); // ["otherarray"]
+console.log("Third item in mixedArr (number):", mixedArr[2]); // 123
+console.log("Fourth item in mixedArr (boolean):", mixedArr[3]); // true
